b0f34a5b58ea9a0ab59ec07f486165dc359059d9
Andras Palfi <andras.palfi@uconn.edu>
2016-10-30 14:20:56 -0400

    Initial Commit

---
+++ SDP_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDP_Swift
+//
+//  Created by Andras Palfi on 10/30/16.
+//  Copyright © 2016 Andras Palfi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SDP_SwiftTests/SDP_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  SDP_SwiftTests.swift
+//  SDP_SwiftTests
+//
+//  Created by Andras Palfi on 10/30/16.
+//  Copyright © 2016 Andras Palfi. All rights reserved.
+//
+
+import XCTest
+@testable import SDP_Swift
+
+class SDP_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
